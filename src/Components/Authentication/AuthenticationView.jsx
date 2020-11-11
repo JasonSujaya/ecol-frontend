ae38c08dadcc1013bd8206e6827bec587f8e833e
jasonsujaya <sujaya.jason@gmail.com>
2020-11-11 19:40:50 +0100

    Added basic sign up form

--- src/Components/Authentication/AuthenticationView.jsx
+++ src/Components/Authentication/AuthenticationView.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
 
 class AuthenticationView extends React.Component {
   render() {
     return (
       <div>
         <LoginForm></LoginForm>
+        <SignUpForm></SignUpForm>
       </div>
     );
   }
---
+++ src/Components/Authentication/SignUpForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class SignUpForm extends React.Component {
+  render() {
+    return <div>Sign Up Stuff</div>;
+  }
+}
+
+export default SignUpForm;
