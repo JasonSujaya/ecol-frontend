6f35a51d2d75680e80f2ad265513dce64d9b3237
jasonsujaya <sujaya.jason@gmail.com>
2020-11-11 22:51:26 +0100

    Added login function that pass the test

--- src/Components/Authentication/authentication.js
+++ src/Components/Authentication/authentication.js
@@ -1,5 +1,27 @@
 import axios from "axios";
 
-export const login = async (_email, _password) => {};
+export const login = async (_email, _password) => {
+  const user = {
+    username: _email,
+    password: _password,
+  };
 
-export const formsNotEmpty = async (_email, _password) => {};
+  const response = await axios.post(
+    `http://127.0.0.1:8000/api/user/login/`,
+    user,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response;
+};
+
+export const formsNotEmpty = (_email, _password) => {
+  if (_email.length != 0 && _password.length != 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
