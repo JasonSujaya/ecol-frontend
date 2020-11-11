ed7c3ee3ac861b6a8a3ac2cd1b42e6157c583886
jasonsujaya <sujaya.jason@gmail.com>
2020-11-12 00:00:01 +0100

    Added login form that pass test

--- src/Components/Authentication/LoginForm.jsx
+++ src/Components/Authentication/LoginForm.jsx
@@ -1,8 +1,77 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import { login, formsNotEmpty } from "./authentication.js";
 
 class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      validForm: false,
+    };
+  }
+
+  onSubmitLogin = async (e) => {
+    e.preventDefault();
+    // let result = await login(this.state.email, this.state.password);
+    let result = login(this.state.email, this.state.password);
+    result
+      .then((response) => {
+        this.setState({ email: "", password: "" });
+        console.log("Called with good");
+      })
+      .catch((error) => {
+        console.log("Called with error");
+      });
+  };
+
   render() {
-    return <div>Login stuff here</div>;
+    return (
+      <Grid container direction="column" justify="center" alignItems="center">
+        <form noValidate autoComplete="off">
+          <div>
+            <TextField
+              required
+              id="standard-required"
+              label="email"
+              className="emailInput"
+              onChange={(e) =>
+                this.setState({
+                  email: e.target.value,
+                  validForm: formsNotEmpty(e.target.value, this.state.password),
+                })
+              }
+              value={this.state.email}
+            />
+            <TextField
+              required
+              id="standard-password-input"
+              label="password"
+              type="password"
+              className="passwordInput"
+              onChange={(e) =>
+                this.setState({
+                  password: e.target.value,
+                  validForm: formsNotEmpty(this.state.email, e.target.value),
+                })
+              }
+              value={this.state.password}
+            />
+            <Button
+              disabled={!this.state.validForm}
+              variant="contained"
+              onClick={this.onSubmitLogin}
+              className="signInButton"
+            >
+              Sign In
+            </Button>
+          </div>
+        </form>
+      </Grid>
+    );
   }
 }
 
