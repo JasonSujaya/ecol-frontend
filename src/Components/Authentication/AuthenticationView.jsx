bf11ce852063d3c01d1b7c677c8b47970c235894
jasonsujaya <sujaya.jason@gmail.com>
2020-11-12 01:22:38 +0100

    Added authentication view to switch between login and signup

--- src/Components/Authentication/AuthenticationView.jsx
+++ src/Components/Authentication/AuthenticationView.jsx
@@ -1,15 +1,55 @@
 import React from "react";
 import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUpForm";
+import Button from "@material-ui/core/Button";
 
 class AuthenticationView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { display: "LogIn" };
+  }
+  showAuthDisplay = () => {
+    if (this.state.display == "LogIn") {
+      return (
+        <div>
+          <LoginForm></LoginForm>
+          <div>
+            Don't have an account yet?
+            <Button
+              onClick={() => {
+                this.changeDisplay("SignUp");
+              }}
+            >
+              SignUp
+            </Button>
+          </div>
+        </div>
+      );
+    } else if (this.state.display == "SignUp") {
+      return (
+        <div>
+          <SignUpForm></SignUpForm>
+          <div>
+            Already registered?
+            <Button
+              onClick={() => {
+                this.changeDisplay("LogIn");
+              }}
+            >
+              Login
+            </Button>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  changeDisplay = (type) => {
+    this.setState({ display: type });
+  };
+
   render() {
-    return (
-      <div>
-        <LoginForm></LoginForm>
-        <SignUpForm></SignUpForm>
-      </div>
-    );
+    return <div>{this.showAuthDisplay()}</div>;
   }
 }
 
