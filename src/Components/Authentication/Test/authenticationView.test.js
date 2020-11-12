4d1f0197eb122cb69fa2d0f1db514c4f12ab6497
jasonsujaya <sujaya.jason@gmail.com>
2020-11-12 14:47:44 +0100

    Added test to make authenication store token when login is called

--- src/Components/Authentication/Test/authenticationView.test.js
+++ src/Components/Authentication/Test/authenticationView.test.js
@@ -1,32 +1,58 @@
-import { createShallow, createMount } from "@material-ui/core/test-utils";
-import LoginForm from "../LoginForm";
-import SignUpForm from "../SignUpForm";
+// import { createShallow, createMount } from "@material-ui/core/test-utils";
+import { shallow, mount } from "enzyme";
 import AuthenticationView from "../AuthenticationView";
 
 describe("Sign In component", () => {
-  let shallow;
-  let mount;
-
-  beforeAll(() => {
-    shallow = createShallow({ dive: true });
-    mount = createMount({ dive: true });
-  });
-
   it("Authentication renders correctly", () => {
     const wrapper = mount(<AuthenticationView />);
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("When display state is changes, expect to find the correct component", () => {
-    //Act
-    const wrapper = mount(<AuthenticationView />);
+  it("When display state is login, expect to login component", () => {
+    //Arrange
+    const wrapper = shallow(<AuthenticationView />);
+
+    // Act
     wrapper.setState({ display: "LogIn" });
 
     // Assert
-    expect(wrapper.contains(<LoginForm />)).toBe(true);
-    expect(wrapper.contains(<SignUpForm />)).toBe(false);
+    expect(wrapper.find("#loginForm")).toHaveLength(1);
+  });
+
+  it("When display state is signup, expect to signUp component", () => {
+    //Arrange
+    const wrapper = shallow(<AuthenticationView />);
+
+    // Act
     wrapper.setState({ display: "SignUp" });
-    expect(wrapper.contains(<LoginForm />)).toBe(false);
-    expect(wrapper.contains(<SignUpForm />)).toBe(true);
+
+    // Assert
+    expect(wrapper.find("#signUpForm")).toHaveLength(1);
+  });
+
+  it("On login expect local storage token to be saved", () => {
+    // Arrange
+    const historyMock = { push: jest.fn() };
+    const wrapper = mount(<AuthenticationView history={historyMock} />);
+    const instance = wrapper.instance();
+
+    // Act
+    instance.onSuccesfullAuthentication("mockTokenValue");
+
+    // Assert
+    expect(localStorage.token).toEqual("mockTokenValue");
+  });
+
+  it("On login, expect to navigate to landing", () => {
+    // Arrange
+    const historyMock = { push: jest.fn() };
+    const wrapper = mount(<AuthenticationView history={historyMock} />);
+    const instance = wrapper.instance();
+
+    // Act
+    instance.onSuccesfullAuthentication("mockTokenValue");
+
+    // Assert
+    expect(historyMock.push.mock.calls[0]).toEqual(["/"]);
   });
 });
